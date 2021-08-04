c8317146fca18b2f3989e66dceb38de349b496de
simi <64702488+simi-hub@users.noreply.github.com>
2021-08-04 00:33:45 -0700

    Src code for asterisks program (#16)
    -squash and merge commit

---
+++ asterisks.java
@@ -0,0 +1,28 @@
+//Program written by Mosinmiloluwa Ojeyomi (Simi)
+//Created on July 21, 2021
+//This program allows a user to build a house with asterisk
+
+public class asterisks
+{
+    public static void main(String[] args)
+    {
+        System.out.println(" " +" " + " " +" "+" "+" "+" "+ "*");
+        System.out.println(" " +" "+" "+" "+" "+"**" + " " +" "+ "**");
+        System.out.println(" " +" "+" "+"**" + " " + " "+" "+" "+ " "+"**");
+        System.out.println(" " +" "+"**" + " " + " " + " "+" "+" "+" "+" "+"**");
+        System.out.println(" " +"**" + " " + " " + " "+" "+" "+" "+" "+" "+" "+"**");
+        System.out.println("*"+"*"+"*"+"*"+"*"+"*"+"*"+"*"+"*"+"*"+"*"+"*"+"*"+"*"+"*");
+        System.out.println("*"+" "+" "+" "+" "+" "+" "+" "+" "+" "+" "+" "+" "+" "+"*");
+        System.out.println("*"+" "+"*"+"*"+"*"+"*"+" "+" "+" "+" "+" "+" "+" "+" "+"*");
+        System.out.println("*"+" "+"* "+" "+""+"*"+" "+" "+" "+" "+" "+" "+" "+" "+"*");
+        System.out.println("*"+" "+"*"+"*"+"*"+"*"+" "+" "+" "+" "+" "+" "+" "+" "+"*");
+        System.out.println("*"+" "+" "+" "+" "+" "+" "+" "+" "+" "+" "+" "+" "+" "+"*");
+        System.out.println("*"+" "+" "+" "+" "+" "+" "+" "+" "+" "+" "+" "+" "+" "+"*");
+        System.out.println("*"+" "+" "+" "+" "+" "+"*"+"*"+"*"+"*"+" "+" "+" "+" "+"*");
+        System.out.println("*"+" "+" "+" "+" "+" "+"*"+" "+" "+"*"+" "+" "+" "+" "+"*");
+        System.out.println("*"+" "+" "+" "+" "+" "+"*"+" "+" "+"*"+" "+" "+" "+" "+"*");
+        System.out.println("*"+" "+" "+" "+" "+" "+"*"+" "+" "+"*"+" "+" "+" "+" "+"*");
+        System.out.println("*"+" "+" "+" "+" "+" "+"*"+" "+" "+"*"+" "+" "+" "+" "+"*");
+        System.out.println("*"+"*"+"*"+"*"+"*"+"*"+"*"+"*"+"*"+"*"+"*"+"*"+"*"+"*"+"*");
+    }
+}
